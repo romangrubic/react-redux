7a945e481b2cd91dd5acf84ec1d99234c4c23540
Roman Grubic <grubic.roman@gmail.com>
2020-04-27 12:08:36 +0000

    Dispatching actions from the component.

--- redux-basics.js
+++ redux-basics.js
@@ -1,38 +1,37 @@
 const redux = require('redux');
 const createStore = redux.createStore;
 
-
 const initialState = {
     counter: 0
 }
+
 // Reducer
 const rootReducer = (state = initialState, action) => {
-    if (action.type === 'INC_COUNTER'){
+    if (action.type === 'INC_COUNTER') {
         return {
             ...state,
             counter: state.counter + 1
         };
-    } 
-    if (action.type === 'ADD_COUNTER'){
+    }
+    if (action.type === 'ADD_COUNTER') {
         return {
             ...state,
             counter: state.counter + action.value
         };
     }
     return state;
-}
+};
 
-// Store 
+// Store
 const store = createStore(rootReducer);
 console.log(store.getState());
 
 // Subscription
 store.subscribe(() => {
-    console.log('[Subscription]', store.getState())
-})
+    console.log('[Subscription]', store.getState());
+});
 
-// Action
+// Dispatching Action
 store.dispatch({type: 'INC_COUNTER'});
 store.dispatch({type: 'ADD_COUNTER', value: 10});
 console.log(store.getState());
-
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -3,7 +3,27 @@ const initialState = {
 }
 
 const reducer = (state = initialState, action) => {
-    return state
+    if (action.type === 'INCREMENT') {
+        return {
+            counter: state.counter + 1
+        }
+    }
+    if (action.type === 'DECREMENT') {
+        return {
+            counter: state.counter - 1
+        }
+    }
+    if (action.type === 'ADD') {
+        return {
+            counter: state.counter + action.value
+        }
+    }
+    if (action.type === 'SUBTRACT') {
+        return {
+            counter: state.counter + action.value
+        }
+    }
+    return state;
 };
 
 export default reducer;
